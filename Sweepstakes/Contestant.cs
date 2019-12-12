705dfc3db0ee0a7892ee96dc5b126b360367191f
Aaron Quasius <aquasius92@gmail.com>
2019-12-12 11:12:20 -0600

    variables for the COntestant class have been created, first name, last name, email address, and registration number have all been accounted for.

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -8,5 +8,17 @@ namespace Sweepstakes
 {
     class Contestant
     {
+        //member variables (has a)
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registerNumber;
+
+        //constructor
+
+
+        //member methods (can do)
+
+
     }
 }
